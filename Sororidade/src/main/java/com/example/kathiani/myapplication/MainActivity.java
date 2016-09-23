21f446fc46e751212c8f3c18072755962f058023
Leila <leilaapsilva@outlook.com.br>
2016-09-23 10:08:02 -0300

    Merge branch 'master' of https://github.com/Kathiani/AppSororidade
    # Conflicts:
    #	.idea/modules.xml
    #	Sororidade/src/main/res/layout/activity_main.xml

--- Sororidade/src/main/java/com/example/kathiani/myapplication/MainActivity.java
+++ Sororidade/src/main/java/com/example/kathiani/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kathiani.myapplication;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mensagem (View View) {
+        showMessage("Teste", "Mensagem");
+        return;
+    }
+
+    //ShowMessage
+    public void showMessage(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+
 }
